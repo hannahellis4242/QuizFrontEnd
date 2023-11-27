cacaebce67b098049d1af8b6a1a792dc76e9d6ee
hannahellis4242 <hannahellis4242@gmail.com>
2023-11-27 22:18:28 +0000

    adding initial files

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import express, { json, urlencoded } from "express";
+import { join } from "path";
+import morgan from "morgan";
+import session from "express-session";
+import routes from "./routes/routes";
+import Quiz from "./model/Quiz";
+
+declare module "express-session" {
+  interface SessionData {
+    quiz?: Quiz;
+  }
+}
+
+const app = express();
+app.use(morgan("combined"));
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "..", "views"));
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, "..", "public")));
+app.use(session({ secret: "secret", resave: false }));
+
+app.use("/", routes);
+
+const port = 8080;
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
